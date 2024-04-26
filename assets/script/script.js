877ab2a7e5d7ff82c1dbae420e01939d52f1aba2
Bizen <117585845+bzn52@users.noreply.github.com>
2024-04-26 09:04:50 +0545

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,26 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+let Section = document.querySelectorAll('section');
+let Navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    Section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        
+        if(top >= offset && top < offset+height) {
+            Navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ' ]').classList.add('active');
+            })
+        }
+    })
+}
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
